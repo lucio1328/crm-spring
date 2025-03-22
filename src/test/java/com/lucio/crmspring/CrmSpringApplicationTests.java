1efcf232ea65a8bcb7ff13bc876aa3d7f017eb70
Lucio Rakotoniaina <153309163+lucio1328@users.noreply.github.com>
2025-03-22 09:30:38 +0100

    structure de base

---
+++ src/test/java/com/lucio/crmspring/CrmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucio.crmspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
